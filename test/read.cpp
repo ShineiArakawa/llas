e0a598e8b7fdea121a429b63cde4fdd51497f3c0
ShineiArakawa <arakawashinei1115@gmail.com>
2024-04-25 01:16:24 +0900

    Update source codes.

--- test/read.cpp
+++ test/read.cpp
@@ -1,8 +1,8 @@
 #include <llas.hpp>
 
 int main(int argc, char** argv) {
-  // const auto& data = llas::read("C:\\Users\\araka\\Projects\\data\\Palac_Moszna.las");
-  const auto& data = llas::read("/home/shinaraka/Projects/llas/sample/G_Sw_Anny.laz");
+   const auto& data = llas::read("C:\\Users\\araka\\Projects\\data\\Palac_Moszna.las");
+  //const auto& data = llas::read("/home/shinaraka/Projects/llas/sample/G_Sw_Anny.laz");
 
   const size_t& nPoints = data->getNumPoints();
   std::cout << "nPoints: " << nPoints << std::endl;
